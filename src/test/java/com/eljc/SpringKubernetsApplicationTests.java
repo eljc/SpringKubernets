17bf965f02a7fa7851e14b85e48ffd6d84182253
eljc <elder.jc@hotmail.com>
2022-07-21 18:42:39 -0300

    montagem do projeto

---
+++ src/test/java/com/eljc/SpringKubernetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eljc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKubernetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
